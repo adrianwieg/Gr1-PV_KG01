2ffd45973e9cffc2a65ed14a0700c8842c5ca29c
adrianwieg <adrian.wieg@web.de>
2021-11-26 11:05:58 +0100

    B.4.1.1 und B.4.1.2 hinzugefügt.

---
+++ B.4.1.1 Hello world/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2 Kaffeeautomat Teil 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char auswahl;
+	system("cls");
+	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n" << "Bitte w\x84hlen Sie aus:\n" << "(k) Kaffee\n" << "(e) Espresso\n" << "(s) Service-Mode\n" << endl;
+	cin >> auswahl;
+	cout << "Sie haben sich f\x81r Auswahl " << auswahl << " entschieden." << endl;
+	system("pause");
+	return 0;
+}
