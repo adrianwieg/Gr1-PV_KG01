4bad9d6f31fe3cc5fb09c827942c8ea3f6cb0e35
adrianwieg <adrian.wieg@web.de>
2021-11-29 10:08:37 +0100

    Formatierung angepasst

--- B.4.1.1 Hello world/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2 Kaffeeautomat Teil 1.cpp
+++ B.4.1.1 Hello world/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2 Kaffeeautomat Teil 1.cpp
@@ -4,7 +4,11 @@ int main()
 {
 	char auswahl;
 	system("cls");
-	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n" << "Bitte w\x84hlen Sie aus:\n" << "(k) Kaffee\n" << "(e) Espresso\n" << "(s) Service-Mode\n" << endl;
+	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n" 
+		    "Bitte w\x84hlen Sie aus:\n" 
+		    "(k) Kaffee\n" 
+		    "(e) Espresso\n" 
+		    "(s) Service-Mode\n";
 	cin >> auswahl;
 	cout << "Sie haben sich f\x81r Auswahl " << auswahl << " entschieden." << endl;
 	system("pause");
