c9e8fb6649fcce214ee356aeb7da5b7ef7d90ddc
adrianwieg <adrian.wieg@web.de>
2021-12-06 13:12:20 +0100

    B.4.1.8- Fehler kommentiert

--- Praktikum Informatik/B.4.18 Typumwandlung/B.4.1.8 Typumwandlungen.cpp
+++ Praktikum Informatik/B.4.18 Typumwandlung/B.4.1.8 Typumwandlungen.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
 	int l;
-	double laenge{ 100.05 };
+	double laenge{ 900.05 };
 	char zeichen;
-	l = laenge;
-	zeichen = laenge;
+	l = laenge; //es wird eine double-Variable in eine int-Variable umgewandelt. Da int keine Nachkommastellen wie beim double versteht/ interpretieren kann, werden die Nachkommastellen gerundet und weggelassen.
+	zeichen = laenge; //es werden nur die ersten 8 Bit der double- Variable an "zeichen" übergeben
+	cout << zeichen;
 	cout << laenge << " " << l << " " << zeichen << endl;
 	string messlatte{ "777.77" };
-	laenge = static_cast<double>(messlatte);
+	laenge = static_cast<double>(messlatte); //Kann nicht mittels static_cast umgewandelt werden, da string kein verwandter Datentyp von double ist
 }
