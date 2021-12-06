702a28d91e537aa7f098d727d2db02cd982c41eb
adrianwieg <adrian.wieg@web.de>
2021-12-06 11:58:34 +0100

    B.4.1.8 Typumwandlungen hinzugefügt

---
+++ Praktikum Informatik/B.4.18 Typumwandlung/B.4.1.8 Typumwandlungen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int l;
+	double laenge{ 100.05 };
+	char zeichen;
+	l = laenge;
+	zeichen = laenge;
+	cout << laenge << " " << l << " " << zeichen << endl;
+	string messlatte{ "777.77" };
+	laenge = static_cast<double>(messlatte);
+}
