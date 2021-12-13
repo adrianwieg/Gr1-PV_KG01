86b23f4349eb194bd243d522fec3cbaec004c0d3
adrianwieg <adrian.wieg@web.de>
2021-12-13 12:02:14 +0100

    Kommentare zu Aufgabe B.4.4.2 hinzugefügt.

--- Praktikum Informatik/B.4.4.2_Schleifen/B.4.4.2_Schleifen.cpp
+++ Praktikum Informatik/B.4.4.2_Schleifen/B.4.4.2_Schleifen.cpp
@@ -3,13 +3,13 @@ int main()
 {
 	int i, k, n;
 	i = 101, k = 5, n = 10;
-	while (i > 0) k = 2 * k; // ...
+	while (i > 0) k = 2 * k; //Es handelt sich um eine Endlosschleife, da die Variable i unverändert bleibt. Es wird nur k unendlich oft verdoppelt. 
 
 	i = 101, k = 5, n = 10;
-	while (i != 0) i = i - 2; // ...
+	while (i != 0) i = i - 2; //Auch hier liegt eine Endlosschleife vor, da der Wert für i NIE genau 0 erreichen kann. I wird nur 1 bzw -1 oder kleiner. Damit wird die Schleife bis ins unendliche fortgesetzt
 
 	i = 101, k = 5, n = 10;
-	while (n != i) // ...
+	while (n != i) //Auch hier liegt eine Endlosschleife vor, da n immer doppelt so groß wie i ist. Somit ist n IMMER ungleich i.
 	{
 		++i;
 		n = 2 * i;	}}
