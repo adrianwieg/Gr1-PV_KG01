1da19498f883bc7a3ed7c71b1354375280051915
adrianwieg <adrian.wieg@web.de>
2021-12-13 12:50:55 +0100

    Zwischenstand bis Teil c) Aufgabe B.4.5 hinzugefügt.

--- Praktikum Informatik/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2_Kaffeeautomat_Teil_1.cpp
+++ Praktikum Informatik/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2_Kaffeeautomat_Teil_1.cpp
@@ -1,43 +1,104 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 using namespace std;
 int main()
 {
 	//Variablendeklaration
-	char auswahl;
+	char eingabe;
+	string produkt;
+	
+	//Vorratsvariablen
+	unsigned int kaffeebohnen	= 1000;
+	unsigned int espressobohnen = 1000;
+	unsigned int wasser			= 5000;
+	unsigned int milch			= 1000;
+	unsigned int zucker			= 500;
 
-	//Clearen des User Interfaces
-	system("cls");
+	//benötigte Mengen
+	const unsigned int ben_kaffeebohnen		= 5;
+	const unsigned int ben_wasser_kaffee	= 125;
+	const unsigned int ben_wasser_espresso	= 25;
+	const unsigned int ben_milch			= 30;
+	const unsigned int ben_zucker			= 3;
 
-	//Textausgabe
-	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n" 
-		    "Bitte w\x84hlen Sie aus:\n" 
-		    "(k) Kaffee\n" 
-		    "(e) Espresso\n" 
-		    "(s) Service-Mode\n";
+	//Preise
+	const double preis_tasse	= 1.0;
+	const double preis_zusatz	= 0.1;
 
-	//Einlesen der Auswahl
-	auswahl = _getch();
+	//Auswahl
+	bool milch;
+	bool zucker;
 
-	//Umwandlung der Auswahl- Buchstaben in einen String und Ausgabe
-	switch (auswahl)
-	{
-	case 'k':
-		cout << "Sie haben sich f\x81r Auswahl Kaffee entschieden.\n";
-		break;
+	while (true)
+	{	
+		//Clearen des User Interfaces
+		system("cls");
 
-	case 'e':
-		cout << "Sie haben sich f\x81r Auswahl Espresso entschieden.\n";
-		break;
+		//Textausgabe
+		cout << "Herzlich Willkommen beim Kaffee-Automaten!\n"
+			"Preis pro Tasse:\n"
+			"Kaffee oder Espresso:  " << preis_tasse << " Euro\n"
+			"Milch oder Zucker:     " << preis_zusatz << " Euro\n\n"
+			"Bitte w\x84hlen Sie aus:\n"
+			"(k) Kaffee\n"
+			"(e) Espresso\n"
+			"(s) Service-Mode\n";
 
-	case 's':
-		cout << "Sie haben sich f\x81r Auswahl Service-Mode entschieden.\n";
-		break;
+		//Einlesen der Auswahl
+		eingabe = _getch();
 
-	default:
-		break;
+		//Umwandlung der Auswahl- Buchstaben in einen String und Ausgabe
+		switch (eingabe)
+		{
+		case 'k':
+			produkt = "Kaffee";
+			break;
+
+		case 'e':
+			produkt = "Espresso";
+			break;
+
+		case 's':
+			produkt = "Service-Mode";
+			break;
+
+		default:
+			break;
+		}
+		cout << "Sie haben sich f\x81r Auswahl " << produkt << " entschieden.\n";
+
+		//Abfrage Zucker
+		cout << "Möchten Sie Zucker (j/n)?";
+		if (_getch() == 'j')
+		{
+			zucker = true;
+		}
+		else if (_getch() == 'n')
+		{
+			zucker = false;
+		}
+		else
+		{
+			cout << "Falsche Eingabe!\n";
+		}
+
+		//Abfrage Milch
+		cout << "Möchten Sie Milch (j/n)?";
+		if (_getch() == 'j')
+		{
+			milch = true;
+		}
+		else if (_getch() == 'n')
+		{
+			milch = false;
+		}
+		else
+		{
+			cout << "Falsche Eingabe!\n";
+			continue;
+		}
 	}
-
 	//Break der Konsolenanwendung
 	system("pause");
 	return 0;
