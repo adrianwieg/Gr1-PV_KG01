e230ceeb8918991af4c6a96b1214a1a34aa410a0
adrianwieg <adrian.wieg@web.de>
2021-12-13 09:56:08 +0100

    Aufgabe B.4.3.1 auf vorhandene .cpp Datei "B.4.2.1 Kaffeeautomat Teil 1" angewendet. Außerdem wurden noch Kommentare ergänzt und cin gegen _getch() ersetzt.

--- Praktikum Informatik/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2_Kaffeeautomat_Teil_1.cpp
+++ Praktikum Informatik/B.4.1.2 Kaffeeautomat Teil 1/B.4.1.2_Kaffeeautomat_Teil_1.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 int main()
 {
+	//Variablendeklaration
 	char auswahl;
+
+	//Clearen des User Interfaces
 	system("cls");
+
+	//Textausgabe
 	cout << "Herzlich Willkommen beim Kaffee-Automaten!\n" 
 		    "Bitte w\x84hlen Sie aus:\n" 
 		    "(k) Kaffee\n" 
 		    "(e) Espresso\n" 
 		    "(s) Service-Mode\n";
-	cin >> auswahl;
-	cout << "Sie haben sich f\x81r Auswahl " << auswahl << " entschieden." << endl;
+
+	//Einlesen der Auswahl
+	auswahl = _getch();
+
+	//Umwandlung der Auswahl- Buchstaben in einen String und Ausgabe
+	switch (auswahl)
+	{
+	case 'k':
+		cout << "Sie haben sich f\x81r Auswahl Kaffee entschieden.\n";
+		break;
+
+	case 'e':
+		cout << "Sie haben sich f\x81r Auswahl Espresso entschieden.\n";
+		break;
+
+	case 's':
+		cout << "Sie haben sich f\x81r Auswahl Service-Mode entschieden.\n";
+		break;
+
+	default:
+		break;
+	}
+
+	//Break der Konsolenanwendung
 	system("pause");
 	return 0;
 }
